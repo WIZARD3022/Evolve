9600525868f7791f44846eb2fb42a759a897db84
WIZARD3022 <vinitsheetal15@gmail.com>
2025-07-13 17:02:04 +0530

    Auto commit

--- map.py
+++ map.py
@@ -79,7 +79,7 @@ class Map:
         for i in range(2, len(self.arr)-2):
             for j in range(2 , len(self.arr[0])-2):
                 if i != 0 and i != len(self.arr) - 1:
-                    rand_val = random.randint(0, 10)
+                    rand_val = random.randint(0, 9)
                     point = 0
                     if j >= 1 and j <= len(self.arr[0]) - 2:
                         if self.arr[i - 1][j - 1] == 1:
@@ -132,7 +132,7 @@ class Map:
                         # if i >= 5 and j >= 7:
                         control = self.cal(i, j)
 
-                        prob = 10 - point * 2.5 + control * 2
+                        prob = 10 - point * 1.5 + control * 1.5
                         if rand_val > prob:
                             self.arr[i][j] = 1
                     else:
