5e6cf75ce93fb8d601d607fb16b572b5b9ed1aee
WIZARD3022 <vinitsheetal15@gmail.com>
2025-07-14 15:48:08 +0530

    Auto commit

--- map.py
+++ map.py
@@ -38,8 +38,8 @@ class Map:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.tiles = [700, 500]  # tile count
-        self.size = [1, 1]   # tile size
+        self.tiles = [350, 250]  # tile count
+        self.size = [2, 2]   # tile size
         self.arr = self.new()  # Initialize the map array
         self.generate_map()
 
