1b3a75f75523115e1dcba0f86ce9164603381ee1
WIZARD3022 <vinitsheetal15@gmail.com>
2025-07-15 15:24:12 +0530

    Auto commit

--- graph.py
+++ graph.py
@@ -19,4 +19,4 @@ class Graph:
 
     def draw(self, screen, Screen_x, Screen_y):
         pygame.draw.rect(screen, GRAY, (Screen_x, Screen_y, self.width, self.height))
-        pygame.draw.line(screen, WHITE, (Screen_x + self.origin_x, Screen_y ), (Screen_x + self.origin_x, Screen_y ),1)
+        pygame.draw.line(screen, WHITE, (Screen_x + self.origin_x, Screen_y ), (Screen_x + self.origin_x, Screen_y + self.height),1)
