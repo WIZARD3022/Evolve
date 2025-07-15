8d36840800b6f8e82929372062b0f0ab782ecff3
WIZARD3022 <vinitsheetal15@gmail.com>
2025-07-15 16:18:15 +0530

    Auto commit

--- graph.py
+++ graph.py
@@ -18,6 +18,12 @@ class Graph:
         self.origin_x = height // 6.5
         self.origin_y = height - height // 6.5
 
+    def text(self, screen, text, x, y):
+        font = pygame.font.SysFont(None, 16)
+        text_surface = font.render(text, True, (0, 255, 0))  # Green text
+        text_rect = text_surface.get_rect(center=(x, y))
+        screen.blit(text_surface, text_rect)
+
     def draw(self, screen, Screen_x, Screen_y):
         pygame.draw.rect(screen, GRAY, (Screen_x, Screen_y, self.width, self.height))
         pygame.draw.line(screen, WHITE, (Screen_x + self.origin_x, Screen_y ), (Screen_x + self.origin_x, Screen_y + self.height),1)
@@ -25,5 +31,8 @@ class Graph:
 
         for i in reversed(range(0, self.width - int(self.origin_x), self.scale)):
             pygame.draw.line(screen, WHITE, (Screen_x + i + self.origin_x, Screen_y + self.origin_y - 5), (Screen_x + i + self.origin_x, Screen_y + self.origin_y + 5), 1)
+            self.text(screen, str(i*self.scale), Screen_x + i + self.origin_x, Screen_y + self.origin_y + 15)
+
         for i in range(0, self.height - int(self.height - self.origin_y), self.scale):
             pygame.draw.line(screen, WHITE, (Screen_x + self.origin_x - 5, Screen_y + i), (Screen_x + self.origin_x + 5, Screen_y + i), 1)
+            self.text(screen, str(i*self.scale), Screen_x + self.origin_x - 15, Screen_y + i)
